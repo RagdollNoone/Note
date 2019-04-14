9a8f4b575d5a6c4ad7195b06514d27e829ce87dc
Dendy <dendy@192.168.0.107>
2019-04-14 23:13:01 +0800

    Automated commit on 2019-04-14 23:13:01 CST

--- autoCommitAndPush.sh
+++ autoCommitAndPush.sh
@@ -1,8 +1,6 @@
-USER='RagdollNoone'
-REPO='Note'
 COMMIT_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S %Z'`
 
 GIT=`which git`
 ${GIT} add --all .
 ${GIT} commit -m "Automated commit on ${COMMIT_TIMESTAMP}"
-${GIT} push git@github.com:username/repo.git daily
+${GIT} push 
